087b3489e1c847f58ac45ab5bb9672f6f3d0ef7a
Laura Xaraba <lauraxaraba@gmail.com>
2025-03-14 14:29:59 -0300

    Adição de teste de falha no login com stub

--- cypress/e2e/login-incorreto.cy.js
+++ cypress/e2e/login-incorreto.cy.js
@@ -2,6 +2,13 @@ describe('Login incorreto na página de login', () => {
 	beforeEach(() => {
 		cy.visit('https://adopet-frontend-cypress.vercel.app/');
 		cy.get('[data-test="login-button"]').click();
+		cy.intercept(
+			'POST',
+			'https://adopet-api-i8qu.onrender.com/adotante/login',
+			{
+				statusCode: 400,
+			}
+		).as('stubPost');
 	});
 
 	it('Deve preencher os campos de login com informações fora do padrão e exibir mensagem de erro para o usuário', () => {
@@ -9,6 +16,14 @@ describe('Login incorreto na página de login', () => {
 		cy.verif_msg_login(
 			'Por favor, verifique o email digitado',
 			'A senha deve conter pelo menos uma letra maiúscula, um número e ter entre 6 e 15 caracteres'
+		);
+	});
+
+	it('Deve falhar mesmo que os campos sejam preenchidos corretamente', () => {
+		cy.login('ana@email.com', 'Senha123');
+		cy.wait('@stubPost');
+		cy.contains('Falha no login. Consulte suas credenciais.').should(
+			'be.visible'
 		);
 	});
 });
