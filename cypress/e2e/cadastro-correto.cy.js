add603f07681a9d94148779698699cc81837a7aa
Laura Xaraba <lauraxaraba@gmail.com>
2025-03-14 10:07:52 -0300

    Formatação de código

--- cypress/e2e/cadastro-correto.cy.js
+++ cypress/e2e/cadastro-correto.cy.js
@@ -1,11 +1,14 @@
 describe('Cadastro correto na plataforma Adopet', () => {
-  it('Deve visitar a página de cadastro e preencher corretamente os campos para cadastrar um novo usuário na plataforma', () => {
-    cy.visit('https://adopet-frontend-cypress.vercel.app/')
-    cy.get('[data-test="register-button"]').click();
-    cy.get('[data-test="input-name"]').type('Ana Clara de Jesus');
-    cy.get('[data-test="input-email"]').type('anacjesus@email.com');
-    cy.get('[data-test="input-password"]').type('Anac123');
-    cy.get('input[name="confirm_password"]').type('Anac123');
-    cy.get('[data-test="submit-button"]').click();
-  })
-})
+	beforeEach(() => {
+		cy.visit('https://adopet-frontend-cypress.vercel.app/');
+		cy.get('[data-test="register-button"]').click();
+	});
+
+	it('Deve visitar a página de cadastro e preencher corretamente os campos para cadastrar um novo usuário na plataforma', () => {
+		cy.get('[data-test="input-name"]').type('Ana Clara de Jesus');
+		cy.get('[data-test="input-email"]').type('anacjesus@email.com');
+		cy.get('[data-test="input-password"]').type('Anac123');
+		cy.get('input[name="confirm_password"]').type('Anac123');
+		cy.get('[data-test="submit-button"]').click();
+	});
+});
--- cypress/e2e/cadastro-incorreto.cy.js
+++ cypress/e2e/cadastro-incorreto.cy.js
@@ -1,12 +1,16 @@
 describe('Cadastro incompleto na plataforma Adopet', () => {
-    it('Deve preencher os campos de cadastro incorretamente eexibir mensagem ao usuário', () => {
-      cy.visit('https://adopet-frontend-cypress.vercel.app/')
-      cy.get('[data-test="register-button"]').click();
-      cy.get('[data-test="input-name"]').type('Ana Clara de Jesus');
-      cy.get('[data-test="submit-button"]').click();
-      cy.contains('É necessário informar um endereço de email').should('be.visible');
-      cy.contains('Crie uma senha').should('be.visible');
-      cy.contains('Repita a senha criada acima').should('be.visible');
-      
-    })
-  })
+	beforeEach(() => {
+		cy.visit('https://adopet-frontend-cypress.vercel.app/');
+		cy.get('[data-test="register-button"]').click();
+	});
+
+	it('Deve preencher os campos de cadastro incorretamente eexibir mensagem ao usuário', () => {
+		cy.get('[data-test="input-name"]').type('Ana Clara de Jesus');
+		cy.get('[data-test="submit-button"]').click();
+		cy.contains('É necessário informar um endereço de email').should(
+			'be.visible'
+		);
+		cy.contains('Crie uma senha').should('be.visible');
+		cy.contains('Repita a senha criada acima').should('be.visible');
+	});
+});
